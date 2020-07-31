9465b97b971b24c3941342e0472bdad9ba42b350
Ion-manden <45092087+Ion-manden@users.noreply.github.com>
2020-07-31 18:22:53 +0200

    code review

--- resources/views/pages/servermainpage.blade.php
+++ resources/views/pages/servermainpage.blade.php
@@ -6,10 +6,10 @@
             <li class="nav-item">
                 <a class="nav-item" href="/">Home Page</a>
             </li>
-            @foreach ($pageDatas as $key => $value)
-            <li class="nav-item">
-                <a href="{{route('serverDataPage', ['serverDataPageid'=>$key, 'serverMainPageid'=> $viewDataPage->id] )}}">{{ $key }}</a>
-            </li>
+            @foreach ($pageData as $key => $value)
+                <li class="nav-item">
+                    <a href="{{route('serverDataPage', ['serverDataPageId'=>$key, 'serverMainPageId'=> $viewDataPage->id] )}}">{{ $key }}</a>
+                </li>
             @endforeach
         </ul>
     </div>
